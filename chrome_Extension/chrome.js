299b2815b147cb153cf714746d59399d85e72c03
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-04-29 13:56:39 -0400

    Still Working on it

--- chrome_Extension/chrome.js
+++ chrome_Extension/chrome.js
@@ -1,26 +1,25 @@
 
 // Assigned variables
-let inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById('input-btn');
-let ulEl = document.getElementById('ul-el');
 let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
+const deleteBtn = document.getElementById("delete-btn");
+// Assigned variables
 
-// function test() {
-//  savedLinks.textContent += "Button Clicked";
-// }
+if (leadsFromLocalStorage) {
+ myLeads = leadsFromLocalStorage
+ render(myLeads)
+}
 
-// How pros do it - below
-inputBtn.addEventListener('click', function() {
- myLeads.push(inputEl.value)
- inputEl.value = '';
- renderLeads();
- // savedLinks.innerHTML += inputEl;
-})
-
-function renderLeads() {
+function render(leads) {
  let listItems = '';
- for (let i = 0; i < myLeads.length; i++) {
-
+ for (let i = 0; i < leads.length; i++) {
+    // const li = document.createElement('li');
+  // li.textContent = myLeads[i]
+  // ulEl.append(li)
+  // or below
   // listItems += '<li><a target="_blank" href="' + myLeads[i] + '">' + myLeads[i] + '</a></li>';
   // or
   listItems += `
@@ -30,15 +29,46 @@ function renderLeads() {
     </a>
    </li>
   `
-
-
-
-  // or below
-  // const li = document.createElement('li');
-  // li.textContent = myLeads[i]
-  // ulEl.append(li)
  }
  ulEl.innerHTML = listItems;
 }
+
+// let myLeads = `["www.example.com"]`;
+/* Turn myLeads to array */
+// myLeads = json.parse(myLeads);
+/* Push string */
+// myLeads.push("www.lead2.com");
+/* Turn Leads to string */
+// myLeads = JSON.stringify(myLeads);
+/* check if string is a string */
+// console.log(typeof myLeads);
+
+/* first local storage */
+// localStorage.setItem("helloWorld", "https://www.hbomax.com");
+// let name = localStorage.getItem("helloWorld");
+// console.log("name");
+// localStorage.clear();
+
+// function test() {
+//  savedLinks.textContent += "Button Clicked";
+// }
+
+deleteBtn.addEventListener('dblclick', function() {
+ localStorage.clear()
+ myLeads = []
+ render(myLeads)
+})
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+
+// How pros do it - below
+inputBtn.addEventListener('click', function() {
+ myLeads.push(inputEl.value)
+ inputEl.value = "";
+ localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+ render(myLeads)
+})
+
+
 
 
